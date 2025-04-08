e8e502f3f35f27b2ba638283deddb18d37f63f41
EnzoRochaBrito <enzo1brich@gmail.com>
2025-04-08 14:15:20 -0300

    finalizado

--- js/contact.js
+++ js/contact.js
@@ -1,24 +1,6 @@
 const sendButton = document.querySelector('form button');
 const formImg = document.getElementById('formImg');
 
-let X = 0;
-let Y = 0;
-
-async function getIndex(event){
-    X += event.movementX
-    Y += event.movementY
-    // console.log(X)
-    console.log(X)
-    console.log(Y)
-    formImg.style.transform = `translate(${X+100}px, ${Y+100}x)`
-}
-
-function mouseOut(event){
-    formImg.style.transform = 'translate(100px, 100px)'
-}
-
-formImg.addEventListener('mouseleave', mouseOut, false)
-formImg.addEventListener('mousemove', getIndex, false)
 
 function checkContactMethod(ev){
     ev.preventDefault()
